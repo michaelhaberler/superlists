0fedb2131ac4a4788cd417c073035f2437dbcdeb
Michael Haberler <michael.haberler@gmx.at>
2014-09-28 18:24:41 +0200

    test for adding to-do items

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
-
+from pprint import pprint
 
 class HomePageTest(TestCase):
 
@@ -14,5 +14,21 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        #pprint(response.content)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
