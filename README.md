1d06d6f8e9a7064949dd3c05daa8725783b68077
Michael Haberler <michael.haberler@gmx.at>
2014-09-29 22:31:21 +0200

    made functional_tests an app, using LiveServerTestCase

--- README.md
+++ README.md
@@ -9,12 +9,11 @@ Usage
 .. code::
     
     python3 manage.py runserver
-    python3 functional_tests.py
-    python3 manage.py test
+    python3 manage.py test lists
+    python3 manage.py test functional_tests
 
 References
 ----------
 * Test-Driven Web Development with Python
+  http://chimera.labs.oreilly.com/books/1234000000754/ch06.html
 * http://uwsgi-docs.readthedocs.org/en/latest/tutorials/Django_and_nginx.html
-
-
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith checks out the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('superlists', self.browser.title)
 
         # She notices the page title and header mention to-do lists
@@ -60,7 +61,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-    #unittest.main()
